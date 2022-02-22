2bc8ebee95055734e4b9f3aebf700638a7478156
viola951019 <100200503+viola951019@users.noreply.github.com>
2022-02-22 19:12:56 +0800

    Create ex.cpp

---
+++ ex.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    printf("Hello Viola \n ");
+    
+    cout <<  "hello viola ";
+    cout <<  "my height is " << 1 << "m " << 70 << "cm";
+
+    return 0;
+}
